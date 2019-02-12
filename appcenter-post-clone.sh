bd6d11bfbffb7c9d240915d1c41b18d749044767
xenotime-india <sandeepkhoj@gmail.com>
2019-02-12 07:17:54 +0530

    fix

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -4,3 +4,4 @@ brew uninstall node@6
 NODE_VERSION="8.12.0"
 curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
 sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
+yarn global add expo-cli
